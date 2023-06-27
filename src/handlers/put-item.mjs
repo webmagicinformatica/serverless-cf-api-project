64c4781d9175f130f2851e211177cb1bc29fa406
webmagicinformatica <56603855+webmagicinformatica@users.noreply.github.com>
2023-06-28 02:05:27 +0530

    Update put-item.mjs

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -2,7 +2,7 @@
 
 // Create a DocumentClient that represents the query to add an item
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
@@ -10,40 +10,35 @@ const ddbDocClient = DynamoDBDocumentClient.from(client);
 const tableName = process.env.SAMPLE_TABLE;
 
 /**
- * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
-export const putItemHandler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+export const getAllItemsHandler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
     }
     // All log statements are written to CloudWatch
     console.info('received:', event);
 
-    // Get id and name from the body of the request
-    const body = JSON.parse(event.body);
-    const id = body.id;
-    const name = body.name;
-
-    // Creates a new item, or replaces an old item with a new item
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
     var params = {
-        TableName : tableName,
-        Item: { id : id, name: name }
+        TableName : tableName
     };
 
     try {
-        const data = await ddbDocClient.send(new PutCommand(params));
-        console.log("Success - item added or updated", data);
-      } catch (err) {
-        console.log("Error", err.stack);
-      }
+        const data = await ddbDocClient.send(new ScanCommand(params));
+        var items = data.Items;
+    } catch (err) {
+        console.log("Error", err);
+    }
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(body)
+        body: JSON.stringify(items)
     };
 
     // All log statements are written to CloudWatch
     console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
     return response;
-};
+}
