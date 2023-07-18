9c8cde3e5b57bfeba4e0491b9fc60354a2d02503
webmagicinformatica <56603855+webmagicinformatica@users.noreply.github.com>
2023-07-18 22:36:39 +0530

    Update put-item.mjs

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -2,7 +2,7 @@
 
 // Create a DocumentClient that represents the query to add an item
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
@@ -10,35 +10,40 @@ const ddbDocClient = DynamoDBDocumentClient.from(client);
 const tableName = process.env.SAMPLE_TABLE;
 
 /**
- * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
  */
-export const getAllItemsHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+export const putItemHandler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
     }
     // All log statements are written to CloudWatch
     console.info('received:', event);
 
-    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    // Get id and name from the body of the request
+    const body = JSON.parse(event.body);
+    const id = body.id;
+    const name = body.name;
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     var params = {
-        TableName : tableName
+        TableName : tableName,
+        Item: { id : id, name: name }
     };
 
     try {
-        const data = await ddbDocClient.send(new ScanCommand(params));
-        var items = data.Items;
-    } catch (err) {
-        console.log("Error", err);
-    }
+        const data = await ddbDocClient.send(new PutCommand(params));
+        console.log("Success - item added or updated", data);
+      } catch (err) {
+        console.log("Error", err.stack);
+      }
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify(body)
     };
 
     // All log statements are written to CloudWatch
     console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
     return response;
-}
+};
